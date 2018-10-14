75ca29363cf942e7f1700352fa5910852c2f8013
Rudi Yardley <contact@rudiyardley.com>
2018-10-14 21:56:54 +0800

    Refactor to be clearer

--- src/client.tsx
+++ src/client.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from '../features/app/App';
+import App from './features/app/App';
 
 export default function main() {
   ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
